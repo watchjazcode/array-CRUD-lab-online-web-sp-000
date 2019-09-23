5c354fbbb62a2448d0f423f4250390a7657df2f3
Jazmin M <jazminmerckel@gmail.com>
2019-09-23 05:52:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,8 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["wow", "I", "am", element]
-  element.pop
+  array = ["wow", "I", "am", "learning"]
+  learning = array.pop
 end
 
 def remove_element_from_start_of_array(array)
