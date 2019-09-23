b57046734055cd8fd6897e5fea3a203fadd91877
Jazmin M <jazminmerckel@gmail.com>
2019-09-23 06:09:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ end
 
 def remove_element_from_start_of_array(array)
   array = ["wow", "I", "am", "learning", "arrays!"]
-  arrays_element = array.shift
+  wow_element = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
