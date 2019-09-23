59ba36fc3fae836eb566859c31f2c248f3e59c86
Jazmin M <jazminmerckel@gmail.com>
2019-09-23 06:01:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,7 +31,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = ["wow", "I", "am", "learning"]
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
