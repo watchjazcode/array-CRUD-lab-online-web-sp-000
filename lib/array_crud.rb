08649d7c321742b473ba3286ecb9781415f1f2bf
Jazmin M <jazminmerckel@gmail.com>
2019-09-23 05:45:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  array = ["dog", "cat", "mouse", "horse"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = []
 end
 
 def add_element_to_start_of_array(array, element)
